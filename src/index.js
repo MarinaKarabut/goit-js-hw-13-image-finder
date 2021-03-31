da363ed367d8efd6103442261e09809728933e17
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-31 23:58:31 +0300

    Добавила открытие большой картинки

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import './styles.css';
 import ApiService from './scripts/apiService.js'
 import cardPhotoTpl from './templates/cardPhoto.hbs'
-// import { alert, error } from'@pnotify/core';
-// import"@pnotify/core/dist/BrightTheme.css"
-// import"@pnotify/core/dist/PNotify.css";
+import * as basicLightbox from 'basiclightbox'
+import 'basiclightbox/dist/basicLightbox.min.css';
+import { alert, error } from'@pnotify/core';
+import"@pnotify/core/dist/BrightTheme.css"
+import"@pnotify/core/dist/PNotify.css";
+
+
+var debounce = require('lodash.debounce');
 
 const gallery = document.querySelector('.gallery')
 const searchField = document.querySelector('.search-field')
 const LoadMoreBtn = document.querySelector('.button')
-var debounce = require('lodash.debounce');
+
+
 
 const apiService = new ApiService()
 searchField.addEventListener('input', debounce(onSearchImg,1000))
 LoadMoreBtn.addEventListener('click', onLoadMore)
+gallery.addEventListener('click', function (e) {
+    if (e.target.nodeName !== 'IMG') {
+      return;
+    }
+    const largeImageURL = e.target.dataset.source
+  if (largeImageURL) {
+    const instance = basicLightbox.create(`
+      <img src="${largeImageURL}" width="1200" height="800">
+  `)
+      instance.show()
+  }
+}
+)
+
 
 function onSearchImg(e) {
     e.preventDefault()
@@ -25,12 +45,16 @@ function onSearchImg(e) {
     })
     
 
-
 }
 
 function onLoadMore() {
- apiService.fetchArticles().then(renderMarkupPhotoCard)
-
+    apiService.fetchArticles().then(renderMarkupPhotoCard)
+    const clientHeight = document.documentElement.clientHeight
+    window.scrollTo({
+    left: 0,
+    top: clientHeight,
+    behavior: 'smooth'
+});
 }
 
 
@@ -43,5 +67,24 @@ function renderMarkupPhotoCard(hits) {
 function clearMarkupPhotoCard() {
     gallery.innerHTML = ''
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { alert, error } from'@pnotify/core';
+// import"@pnotify/core/dist/BrightTheme.css"
+// import"@pnotify/core/dist/PNotify.css";
+
 
 
--- src/scripts/apiService.js
+++ src/scripts/apiService.js
@@ -16,12 +16,20 @@ export default class ApiService {
         
         const url = `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=20930495-600a23973a3be0872b747cdc1`
         return fetch(url)
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Совпадений не найдено. Введите более детальные критерии")
+                }
+                return response.json()
+            })
             .then(({hits}) => {
                 this.incrementPage();
                 return hits;
                 
             })
+            
+        
+        
         
     
     }
