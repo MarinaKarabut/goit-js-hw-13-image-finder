5770f72b672660e5455f71d950a2c76efe37d274
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-04-03 14:12:12 +0300

    спрятала кнопку

---
+++ src/scripts/main.js
@@ -0,0 +1,93 @@
+
+import ApiService from './apiService'
+import cardPhotoTpl from '../templates/cardPhoto.hbs'
+import * as basicLightbox from 'basiclightbox'
+import 'basiclightbox/dist/basicLightbox.min.css';
+import {  error } from '@pnotify/core';
+import"@pnotify/core/dist/BrightTheme.css"
+import "@pnotify/core/dist/PNotify.css";
+
+
+
+
+var debounce = require('lodash.debounce');
+const LoadMoreBtn = document.querySelector('.button')
+const gallery = document.querySelector('.gallery')
+const searchField = document.querySelector('.search-field')
+
+
+
+
+const apiService = new ApiService()
+searchField.addEventListener('input', debounce(onSearchImg,1000))
+LoadMoreBtn.addEventListener('click', onLoadMore)
+gallery.addEventListener('click', function (e) {
+    if (e.target.nodeName !== 'IMG') {
+      return;
+    }
+    const largeImageURL = e.target.dataset.source
+  if (largeImageURL) {
+    const instance = basicLightbox.create(`
+      <img src="${largeImageURL}" width="1200" height="800">
+  `)
+      instance.show()
+  }
+}
+)
+
+function onSearchImg(e) {
+    e.preventDefault()
+    apiService.query = e.target.value
+
+    apiService.resetPage()
+    apiService.fetchArticles().then(hits => {
+         if (!hits.length) {
+            onError('"Not found. Please enter a more specific query!"')
+        }
+        clearMarkupPhotoCard()
+        renderMarkupPhotoCard(hits)
+        LoadMoreBtn.classList.remove('hidden')
+        
+    })
+}
+
+function onLoadMore() {
+    const lastElement = document.querySelector('.gallery .gallery-photo-item:last-child')
+
+    apiService.fetchArticles().then((result) => {
+        renderMarkupPhotoCard(result);
+        
+
+        if (lastElement) {
+            window.scrollTo({
+                top: lastElement.offsetTop + lastElement.clientHeight,
+                behavior: "smooth"
+            });        
+        }
+
+    })
+    
+}
+
+
+function renderMarkupPhotoCard(hits) {
+    const markup = cardPhotoTpl(hits)
+    gallery.insertAdjacentHTML('beforeend', markup)
+}
+
+function clearMarkupPhotoCard() {
+    gallery.innerHTML = ''
+}
+
+
+
+function onError(message) {
+    error({
+        text: message
+    })
+ }
+
+
+
+
+
